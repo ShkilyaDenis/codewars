14813de55a5aadf6dab445ec7db91235600be6dc
ShkilyaDenisPython <shkilyadenis@gmail.com>
2024-11-15 15:02:47 +0400

    Create сonvert_boolean_values_to_strings_'Yes'_or_'No'.py

---
+++ 8kyu/сonvert_boolean_values_to_strings_'Yes'_or_'No'/сonvert_boolean_values_to_strings_'Yes'_or_'No'.py
@@ -0,0 +1,8 @@
+def bool_to_word(boolean):
+    # Проверяем, является ли boolean True
+    if boolean:
+        # Если True, возвращаем "Yes"
+        return "Yes"
+    else:
+        # Если False, возвращаем "No"
+        return "No"
