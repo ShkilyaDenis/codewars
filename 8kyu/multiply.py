7846bfd970efb6f68d238989e1d0120590d00554
ShkilyaDenisPython <shkilyadenis@gmail.com>
2024-11-15 18:06:29 +0400

    Update multiply.py

--- 8kyu/multiply.py
+++ 8kyu/multiply.py
@@ -1,4 +1,12 @@
+#This code does not execute properly. Try to figure out why.
+
+
 def multiply(a, b):
+    # Выполняем умножение
     return a * b
 
-print(multiply(3, 5))
+
+# Тесты
+print(multiply(3, 5))  # Ожидаемый вывод: 15
+print(multiply(-2, 4))  # Ожидаемый вывод: -8
+print(multiply(3.5, 2))  # Ожидаемый вывод: 7.0
