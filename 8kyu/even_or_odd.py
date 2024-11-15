c0b5e284f72dac689c1d8ca734cdd249c0761d54
ShkilyaDenisPython <shkilyadenis@gmail.com>
2024-11-15 18:06:32 +0400

    Update even_or_odd.py

--- 8kyu/even_or_odd.py
+++ 8kyu/even_or_odd.py
@@ -1,8 +1,17 @@
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
 def even_or_odd(number):
+    # Если остаток от деления равен 0, число четное
     if number % 2 == 0:
+        # Возвращаем "Even" для четного числа
         return "Even"
     else:
+        # Возвращаем "Odd" для нечетного числа
         return "Odd"
 
-result = even_or_odd(5)
-print(result)
+
+# Тесты
+print(even_or_odd(4))  # "Even"
+print(even_or_odd(0))  # "Even"
+print(even_or_odd(-3))  # "Odd"
