96ddf76551f2110d030a2da7a547e968b1245731
ShkilyaDenisPython <shkilyadenis@gmail.com>
2024-11-15 18:06:23 +0400

    Update reversed_stringsct.py

--- 8kyu/reversed_stringsct.py
+++ 8kyu/reversed_stringsct.py
@@ -1,9 +1,14 @@
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+
 def reverse_string(s):
     #Мы используем срез[::-1], который позволяет пройти по строке в обратном порядке, создавая новую строку.
     return s[::-1]
     # В результате функция возвращает строку, но символы идут в обратном порядке.
 
 
-# Примеры использования
+# Тесты
 print(reverse_string('world'))  # Вывод: 'dlrow'
 print(reverse_string('Hello'))   # Вывод: 'olleH'
