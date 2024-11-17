4f025d03b97ae8160194bc1acb7d9d63a4253ab9
ShkilyaDenisPython <shkilyacrypto@gmail.com>
2024-11-17 12:29:30 +0400

    Update abbreviate_a_two_word_name.py

--- 8kyu/abbreviate_a_two_word_name.py
+++ 8kyu/abbreviate_a_two_word_name.py
@@ -1,4 +1,17 @@
-# def abbrev_name(name):
-#     return len.name.upper()[0]
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+# It should look like this:
+# Sam Harris => S.H
+# patrick feeney => P.F
 
 
+def abbrev_name(name):
+    # Разбиваем имя на слова
+    first_name, last_name = name.split()
+    # Берем первые буквы, делаем их заглавными и объединяем через точку
+    return f"{first_name[0].upper()}.{last_name[0].upper()}"
+
+
+# Тесты
+print(abbrev_name("Sam Harris"))  # Вывод: S.H
+print(abbrev_name("patrick feeney"))  # Вывод: P.F
